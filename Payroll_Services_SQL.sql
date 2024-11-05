072e15e2da3f7138cfb008eb3faf2d84e22fef39
dipkumar patil <dipkumarpa2020@gmail.com>
2024-11-05 11:44:18 +0530

    Create employee_payroll Table

--- Payroll_Services_SQL.sql
+++ Payroll_Services_SQL.sql
@@ -7,3 +7,15 @@ FROM sys.databases
 WHERE name = 'payroll_service';
 
 USE payroll_service;
+
+
+
+CREATE TABLE employee_payroll (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(50),
+    salary DECIMAL(18, 2),
+    start_date DATE
+);
+
+
+
