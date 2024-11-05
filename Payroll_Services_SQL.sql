a2749b06550b591ee8a101be8c8289f8daa4a3fd
dipkumar patil <dipkumarpa2020@gmail.com>
2024-11-05 13:45:48 +0530

    Retrive data from employee_payroll Table

--- Payroll_Services_SQL.sql
+++ Payroll_Services_SQL.sql
@@ -1,14 +1,11 @@
 
 CREATE DATABASE payroll_service;
 
-
 SELECT name 
 FROM sys.databases 
 WHERE name = 'payroll_service';
 
 USE payroll_service;
-
-
 
 CREATE TABLE employee_payroll (
     id INT IDENTITY(1,1) PRIMARY KEY,
